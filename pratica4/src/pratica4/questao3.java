33dea7f1ef1ed35eaddcb172459eaf8de7c0a0e1
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2022-05-04 21:31:02 -0300

    questao 3

--- pratica4/src/pratica4/questao3.java
+++ pratica4/src/pratica4/questao3.java
@@ -6,52 +6,57 @@ import java.util.ArrayList;
 public class questao3 {
 
 	public static void main(String[] args) {
-		Scanner entrance = new Scanner(System.in);
+		Scanner escreva = new Scanner(System.in);
 		
-		ArrayList<String> itens = new ArrayList();
+		String item, autor, data, descricao, local;
+		int options, menu;
 		
-		int opcao;
-		String nomeItem;
-		String autor;
-		String local;
-		String data;
-		String descricao;
+		ArrayList<String> arrayItens = new ArrayList();
 		
 		
-		System.out.println("Escolha uma das opcoes abaixo: ");
-		System.out.println("(1) Cadastrar Item Perdido");
-		System.out.println("(2) Buscar Item");
-		System.out.println("(3) Sair");
+		for(int i=0; i<10; i++) {
+			
+		System.out.println("Escolha sua opção: \n1 - Cadastrar Item Perdido \n2 - Buscar Item \n3 - Sair");
+		
+		options = escreva.nextInt();
+		
+		 switch(options) {
+		 
+		 	 case 1:
+				 System.out.println("Informe o item: ");
+				 item = escreva.next();
+				 System.out.println("Informe o autor: ");
+				 autor = escreva.next();
+				 System.out.println("Informe a data: ");
+				 data = escreva.next();
+				 System.out.println("Informe a descricao: ");
+				 descricao = escreva.next();
+				 System.out.println("Informe o local: ");
+				 local = escreva.next();
+			 
+				 arrayItens.add(item+" / "+autor+" / "+data+" / "+descricao+" / "+local);
+			 break;
+			 
+		 	 case 2:
+		 		System.out.printf("Listando itens cadastrados\n");
+		 	    int n = arrayItens.size();
+		 	    for (i=0; i<n; i++) {
+		 	      System.out.printf("Posição %d- %s\n", i, arrayItens.get(i));
+		 	    }
+		 	    System.out.println("Encontrou seu item? Informe a posição para retira-lo \nCaso nao tenha encontrado seu item digite -1");
+		 	    menu = escreva.nextInt();
+		 	    if(menu<=10 && menu>=0) {		 	    	
+		 	    System.out.println("Item Encontrado na posição "+menu+" e Recuperado com Sucesso!");
+		 	    }else if(menu==-1) {
+		 	    	System.out.println("Retornando ao menu principal");
+		 	    }else
+		 	    	System.out.println("Opção inválida!");
+		 	    
+		 	 case 3:
+		 		 System.exit(0);
 
-		opcao = entrance.nextInt();
-		
-		switch(opcao) {
-		case 1: 
-			
-			
-			System.out.println("Digite o nome do item perdido: ");
-			nomeItem = entrance.next();
-			
-			System.out.println("Digite o nome do autor do iten: ");
-			autor = entrance.next();
-			
-			System.out.println("Digite o nome do autor do iten: ");
-			autor = entrance.next();
-			
-			System.out.println("Digite o local do iten: ");
-			local = entrance.next();
-			
-			System.out.println("Digite a data do iten: ");
-			data = entrance.next();
-			
-			System.out.println("Digite a descricao do iten: ");
-			descricao = entrance.next();
-			
-			
-		case 2:
-			
-		case 3:
-		}
+		 	}
+		 }	
 	}
 
 }
